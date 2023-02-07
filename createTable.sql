81012f8638e762d3346e0b8c1bfc730182d93a6d
Isadora <dorisperdigao@gmail.com>
2023-02-07 12:54:03 -0300

    fixes sql instructions

--- createTable.sql
+++ createTable.sql
@@ -1,19 +1,19 @@
 CREATE TYPE OS AS ENUM ('WINDOWS', 'LINUX', 'MACOS');
 
 CREATE TABLE developer_infos (
-	id SERIAL,
+	id SERIAL PRIMARY KEY,
 	developerSince DATE NOT NULL,
 	preferredOS OS NOT NULL
 );
 
 CREATE TABLE developers (
-	id SERIAL,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT null
 );
 
 CREATE TABLE projects (
-	"id" SERIAL,
+	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(50) NOT NULL,
 	"description" TEXT NOT NULL,
 	"estimatedTime" VARCHAR(20) NOT NULL,
@@ -23,11 +23,17 @@ CREATE TABLE projects (
 );
 
 CREATE TABLE technologies (
-	id SERIAL,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE projects_technologies (
-	id SERIAL,
+	id SERIAL PRIMARY KEY,
 	addedIn DATE NOT NULL 
 );
+
+ALTER TABLE developers 
+ADD COLUMN "developerInfoId" INTEGER UNIQUE;
+
+ALTER TABLE developers 
+ADD FOREIGN KEY ("developerInfoId") REFERENCES developer_infos(id);

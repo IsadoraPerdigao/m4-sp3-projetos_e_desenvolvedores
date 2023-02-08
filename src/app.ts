f83947ae03abee2251bba7b258f1dba0734cffa5
Isadora <dorisperdigao@gmail.com>
2023-02-08 16:09:24 -0300

    feature: creates route to list all developers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 import { startDataBase } from "./database"
-import { createDeveloperInfo, createNewDeveloper } from "./logics/devLogics"
+import { createDeveloperInfo, createNewDeveloper, listAllDevelopers } from "./logics/devLogics"
 import { checkIfDeveloperExists, checkIfEmailExists, checkRequiredKeysCreateDeveloper, checkRequiredKeysDeveloperInfos, removeExtraKeysCreateDeveloper, removeExtraKeysDeveloperInfos } from "./middlewares/devValidations"
 
 const app: Application = express()
@@ -8,6 +8,7 @@ app.use(express.json())
 
 app.post("/developers", checkIfEmailExists, checkRequiredKeysCreateDeveloper, removeExtraKeysCreateDeveloper, createNewDeveloper)
 app.post("/developers/:id/infos", checkIfDeveloperExists, checkRequiredKeysDeveloperInfos, removeExtraKeysDeveloperInfos, createDeveloperInfo)
+app.get("/developers", listAllDevelopers)
 
 app.listen(3000, async () => {
     await startDataBase()
